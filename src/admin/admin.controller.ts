3ec942c651659aee31670624c510ba355fb1bff2
Chung-MinJun <myhome722@kyonggi.ac.kr>
2024-02-16 04:59:47 +0900

    3.1 commit
    remove user update

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,28 +1,24 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { SearchUsersDto } from './dto/search-users.dto';
 
 @Controller('admin')
 export class AdminController {
-    constructor(private readonly adminService:AdminService) { }
-
-    @Get()
-    findAll(){
-        return this.adminService.findAll();
-    }
+    constructor(private readonly adminService: AdminService) { }
 
     @Post(':uid')
-    findOne(@Param('uid') uid: string){
+    findOne(@Param('uid') uid: string) {
         return this.adminService.findOne(+uid);
     }
 
     @Patch(':uid')
-    userUpdate(@Param('uid') uid:string, @Body() updateUserDto:UpdateUserDto) {
-        return this.adminService.updateRole(+uid,updateUserDto);
+    userUpdate(@Param('uid') uid: string, @Body() updateUserDto: UpdateUserDto) {
+        return this.adminService.updateRole(+uid, updateUserDto);
     }
 
-    @Delete(':uid')
-    remove(@Param('uid') uid:string,@Body() ){
-        return this.adminService.remove(,+uid);
+    @Delete(':auid')
+    remove(@Param('auid') auid: string, @Body() uid: string) {
+        return this.adminService.remove(+auid, +uid);
     }
 }
